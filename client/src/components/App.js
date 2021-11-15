8c9f3df9cd26297e69e8d667a800ab59d84b79bc
haditorabi <haditorabi1984@gmail.com>
2021-11-15 10:25:09 -0500

    create navbar and menus

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from "react-router-dom";
+import NavBar from "./views/NavBar/NavBar";
 
 
 function App() {
   return (
     <Suspense fallback={(<div>Loading...</div>)}>
+      <NavBar />
+
       <div className="content_wrapper" style={{ paddingTop: '75px', minHeight: 'calc(100vh - 80px)' }}>
         <Routes>
         </Routes>
