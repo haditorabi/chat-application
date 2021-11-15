302f551552256ae16ba4dc07ae77e700f41164a4
haditorabi <haditorabi1984@gmail.com>
2021-11-15 00:05:16 -0500

    Create index.js with mongoose and basic conf

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const server = require("http").createServer(app);
+const config = require("./config/key");
+
+const mongoose = require("mongoose");
+const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+const port = process.env.PORT || 5000
+
+server.listen(port, () => {
+  console.log(`Server Running at ${port}`)
+});
