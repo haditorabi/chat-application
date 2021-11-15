756c822102bec737eb305c48d3db95de15d7d85d
haditorabi <haditorabi1984@gmail.com>
2021-11-15 01:52:04 -0500

    setup redux folders

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,27 @@ import './index.css';
 import App from './components/App.js';
 import reportWebVitals from './reportWebVitals';
 
+import Reducer from './reducers';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider
+    store={createStoreWithMiddleware(
+        Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
