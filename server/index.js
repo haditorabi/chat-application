a75cd0fb9ab9511a143b82f6f1db193154160fd8
haditorabi <haditorabi1984@gmail.com>
2021-11-15 00:07:43 -0500

    config socket.io

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
 const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 const config = require("./config/key");
 
 const mongoose = require("mongoose");
@@ -14,6 +15,29 @@ const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUn
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+io.on("connection", socket => {
+  socket.on("Input Chat Message", msg => {
+    connect.then(db => {
+      try {
+          let chat = new Chat({ message: msg.chatMessage, sender:msg.userId, type: msg.type })
+          chat.save((err, doc) => {
+            console.log(doc)
+            if(err) return res.json({ success: false, err })
+
+            Chat.find({ "_id": doc._id })
+            .populate("sender")
+            .exec((err, doc)=> {
+                return io.emit("Output Chat Message", doc);
+            })
+          })
+      } catch (error) {
+        console.error(error);
+      }
+    })
+   })
+})
+
 const port = process.env.PORT || 5000
 
 server.listen(port, () => {
